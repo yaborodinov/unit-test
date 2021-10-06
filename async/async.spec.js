d284b8d893fd9171a1ffe923a0d5be3d672e1eb3
Alexandr <yaborodinov@gmail.com>
2021-10-06 19:01:59 +0300

    testing async code

---
+++ async/async.spec.js
@@ -0,0 +1,14 @@
+const Ajax = require('./async')
+
+describe('Ajax: echo', () => {
+  test('should return value async', async () => {
+    const result = await Ajax.echo('some data')
+    expect(result).toBe('some data')
+  })
+  
+  test('should return value with promise', () => {
+    return Ajax.echo('some data').then(data => {
+      expect(data).toBe('some data')
+    })
+  })
+})
